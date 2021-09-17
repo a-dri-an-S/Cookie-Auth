3543cee12054705a64e1b2606fa011649d0c5e02
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-09-16 22:04:21 -0700

    added error handling to LoginForm

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -6,15 +6,25 @@ const LoginForm = () => {
 
     const [email, setEmail] = useState("Shanna@melissa.tv")
     const [password, setPassword] = useState("anastasia.net")
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const showError = err => {
+        console.log(err)
+        const error = err.response && err.response.data || err.message;
+        setError( error );
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setError('');
+        setLoading(true);
         loginUser(email, password)
         .then(() => {
             Router.push("/profile")
         })
+        .catch(showError)
     }
-
 
     return ( 
         <form onSubmit={handleSubmit}>
@@ -36,7 +46,10 @@ const LoginForm = () => {
                     onChange={(e) => setPassword(e.target.value)}
                 />
             </div>
-            <button type="submit ">Submit</button>
+            <button disabled={loading} type="submit ">
+                {loading ? "Sending" : "Submit"}
+            </button>
+            {error && <div>{error}</div>}
         </form>
     );
 }
