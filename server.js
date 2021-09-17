b6f4ef2c817fec4bf01d7c77f09e6eaff1432f08
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-09-16 20:21:33 -0700

    set up user authentication + added to login post route

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const next = require("next");
 const express = require("express");
+const axios = require("axios");
 
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3000;
 const app = next({ dev });
-
 const handle = app.getRequestHandler();
+
+const AUTH_USER_TYPE = 'authenticated';
+const authenticate = async (email, password) => {
+    await axios.get('https://jsonplaceholder.typicode.com/users');
+    return DataTransfer.find(user => {
+        if (user.email === email && user.website === password) {
+            return user;
+        }
+    })
+}
 
 app.prepare().then(() => {
     const server = express();
@@ -14,7 +24,16 @@ app.prepare().then(() => {
 
     server.post('/api/login', (req, res) => {
         const { email, password } = req.body;
-        authenticate(email, password);
+        const user = await authenticate(email, password);
+        if (!user) {
+            return res.status(403).send("Invalid email or password");
+        }
+        const userData = {
+            name: user.name,
+            email: user.email,
+            type: AUTH_USER_TYPE
+        }
+        res.json(userData);
     });
 
     server.get("*", (req, res) => {
